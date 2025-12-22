19c811946c7dbaf3495d28a361052bce4cd372c6
theresa-de-ocampo <ma_theresa7@yahoo.com>
2025-12-22 13:23:35 +0800

    chore: Upgrade next, react

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,7 +4,7 @@ import loader from "@/assets/loader.gif";
 export default function Loading() {
   return (
     <main className="h-dvh flex-center">
-      <Image src={loader} alt="Loading" height={150} width={150} />
+      <Image src={loader} alt="Loading" height={150} width={150} unoptimized />
     </main>
   );
 }
